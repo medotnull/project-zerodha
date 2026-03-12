5eeee86797ac658369a5003261c7cefaa96c6a7c
medotnull <meenalbansodbmw@gmail.com>
2026-03-12 23:53:30 +0530

    function fill order

--- src/index.ts
+++ src/index.ts
@@ -74,3 +74,50 @@ app.post("/order", (req: any, res: any) => {
         filledQuantity: quantity - remainingQty
     })
 })
+
+app.get("/quote", (req: any, res: any) => {
+    // todo balance
+})
+
+function flipBalance(side: string, price: number, quantity: number, userId: string): number {
+    // todo balance
+}
+
+function fillOrders(side: string, price: number, quantity: number, userId: string): number {
+    let remainingQty = quantity;
+
+    if(side === "buy"){
+        for (let i = asks.length - 1; i >=0; i--){
+            if (asks[i].price <= price) {
+                continue;
+            }
+            if (ask[i].quantity <= remainingQuantity){
+                asks[i].quantity -= remainingQuantity;;
+                flipBalance(asks[i].userId, userId, asks[i].quantity, price);
+                return 0;
+            } else {
+                remainingQuantity -= asks[i]. quantity;
+                flipBalance(asks[i].userId, userId, asks[i].quantity, price);
+                asks.pop();
+            }
+        } else {
+            for (let i = bids.length - 1; i >=0; i--){
+                if (bids[i].price >= price) {
+                    continue;
+                }
+                if (bids[i].quantity <= remainingQuantity){
+                    bids[i].quantity -= remainingQuantity;
+                    flipBalance(userId, bids[i].userId, remainingQuantity, price);
+                    return 0;
+                } else {
+                    remainingQuantity -= bids[i].quantity;
+                    flipBalance(userId, bids[i].userId, bids[i].quantity, price);
+                    bids.pop();
+                }
+            }
+        }
+
+        return remainingQuantity;
+
+    }
+}
